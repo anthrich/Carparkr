47d479b998383131e4251d6e70c7b4bbf04bc138
Anth Richardson <anthrblahblah@gmail.com>
2026-07-21 22:30:33 +0100

    Implements EfCoreCarParkRepository

--- Carparkr.Persistence/EfCoreCarParkRepository.cs
+++ Carparkr.Persistence/EfCoreCarParkRepository.cs
@@ -1,16 +1,36 @@
 ﻿using Carparkr.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Carparkr.Persistence;
 
 public class EfCoreCarParkRepository : ICarParkRepository
 {
-    public Task Save(CarPark carPark)
+    private readonly CarParkContext _context = new();
+
+    public async Task Save(CarPark carPark)
     {
-        throw new NotImplementedException();
+        _context.CarParks.Add(carPark);
+        await _context.SaveChangesAsync();
     }
 
     public Task<List<CarPark>> Get()
     {
-        throw new NotImplementedException();
+        return _context.CarParks.ToListAsync();
+    }
+}
+
+internal sealed class CarParkContext : DbContext
+{
+    public DbSet<CarPark> CarParks => Set<CarPark>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase("CarParks");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CarPark>().Property<int>("Id");
+        modelBuilder.Entity<CarPark>().HasKey("Id");
     }
 }

07b8a1cbf5e4456d38fcb03f95d81256c9614eee
Anth Richardson <anthrblahblah@gmail.com>
2026-07-21 20:22:18 +0100

    CarkPark.Allocate: increments full spaces

---
+++ Carparkr.Domain.Tests/WhenAllocatingASpace.cs
@@ -0,0 +1,16 @@
+﻿namespace Carparkr.Domain.Tests;
+
+public class WhenAllocatingASpace
+{
+    private CarPark _carpark = new();
+    
+    [Fact]
+    public void It_increments_the_number_of_full_spaces()
+    {
+        // Act
+        _carpark.AllocateSpace("RA73 XRF");
+        
+        // Assert
+        Assert.Equal(1, _carpark.GetSpaceSummary().FullSpaces);
+    }
+}
--- Carparkr.Domain/CarPark.cs
+++ Carparkr.Domain/CarPark.cs
@@ -2,8 +2,15 @@
 
 public class CarPark
 {
+    private List<string> _allocatedVehicleRegistrations = new();
+    
     public SpaceSummary GetSpaceSummary()
     {
-        return new SpaceSummary(100, 0, 100);
+        return new SpaceSummary(100, _allocatedVehicleRegistrations.Count, 100);
+    }
+
+    public void AllocateSpace(string vehicleRegistration)
+    {
+        _allocatedVehicleRegistrations.Add(vehicleRegistration);
     }
 }

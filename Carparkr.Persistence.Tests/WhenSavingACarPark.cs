b097df937f7bb8085b4faecfa8e74ba5010ad611
Anth Richardson <anthrblahblah@gmail.com>
2026-07-22 10:38:49 +0100

    EfCoreCarParkRepository.Save: Ensure parked vehicles are saved

--- Carparkr.Persistence.Tests/WhenSavingACarPark.cs
+++ Carparkr.Persistence.Tests/WhenSavingACarPark.cs
@@ -19,4 +19,20 @@ public class WhenSavingACarPark
         var carParks = await _carparkRepository.Get();
         Assert.NotEmpty(carParks);
     }
+
+    [Fact]
+    public async Task The_parked_vehicles_are_saved()
+    {
+        // Arrange
+        var carPark = new CarPark();
+        carPark.AllocateSpace("NU76 JJC", DateTime.UtcNow, Size.Medium);
+        
+        // Act
+        await _carparkRepository.Save(carPark);
+        
+        // Assert
+        var carParks = await _carparkRepository.Get();
+        var exitResult = carParks.First().ExitVehicle("NU76 JJC", DateTime.UtcNow.AddMinutes(1));
+        Assert.Equal(0.20m, exitResult.Charge);
+    }
 }
